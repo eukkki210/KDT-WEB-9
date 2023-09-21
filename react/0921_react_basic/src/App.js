3b8be36450b4ae87ec0af3ab2ceb85d5dc28af4e
eukkki210 <ysg980423@gmail.com>
2023-09-21 18:45:08 +0900

    study: react usestate 공부

---
+++ react/0921_react_basic/src/App.js
@@ -0,0 +1,19 @@
+import Practice1 from "./Practice1";
+import Practice2 from "./Practice2";
+import Practice3 from "./Practice3";
+import Practice4 from "./Practice4";
+import Practice5 from "./Practice5";
+
+function App() {
+  return (
+    <>
+      {/* <Practice1 /> */}
+      {/* <Practice2 /> */}
+      {/* <Practice3 /> */}
+      {/* <Practice4 /> */}
+      <Practice5 />
+    </>
+  );
+}
+
+export default App;
